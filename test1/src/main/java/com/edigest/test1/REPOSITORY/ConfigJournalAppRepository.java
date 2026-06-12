16631a098145613432d4fe6e716cac5caf7bb298
Sahibpreet Singh <157112392+Sahibpreetsingh2@users.noreply.github.com>
2026-06-12 08:48:24 +0530

    Delete test1 directory

--- test1/src/main/java/com/edigest/test1/REPOSITORY/ConfigJournalAppRepository.java
+++
@@ -1,9 +0,0 @@
-package com.edigest.test1.REPOSITORY;
-
-import com.edigest.test1.Entity.ConfigJournalAppEntity;
-import com.edigest.test1.Entity.User;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ConfigJournalAppRepository extends MongoRepository<ConfigJournalAppEntity, ObjectId> {
-}
